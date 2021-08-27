8dd668398d4f85aeaa56cabc68192f1e3f11f49f
aman706 <76611203+aman706@users.noreply.github.com>
2021-08-27 20:16:50 +0530

    Update index.ts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,12 +1,12 @@
 import bot from '../bot';
-import { Start } from './start';
+import { Startvc } from './startvc';
 import { JioSaavnPlay } from './jiosaavn';
 import { YTPlay } from './youtube';
 import { Pause, Resume, Skip, Stop } from './controls';
 import { QueueList } from './queue';
 
 export const InitHandlers = (): void => {
-    bot.use(Start);
+    bot.use(Startvc);
     bot.use(JioSaavnPlay);
     bot.use(YTPlay);
     bot.use(Pause);
