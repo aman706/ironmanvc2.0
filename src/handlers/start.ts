99b26b0547c370a4ff5ced0113ef1306be7290e6
aman706 <76611203+aman706@users.noreply.github.com>
2021-08-27 20:07:04 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -4,10 +4,10 @@ export const Start = Composer.command('start', async (ctx) => {
     let text =
         `Hi <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name} ${ctx.from.last_name}</a>\n` +
         `ʜᴏɪ, ɪ ᴄᴀɴ ᴘʟᴀʏ ꜱᴏɴɢꜱ ɪɴ ɢʀᴘ ᴠɪᴀ ꜱᴀᴀᴠɴ.\n` +
-        `ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ᴀɴᴅ ᴀʙᴏᴜᴛ ᴄᴍᴅꜱ ᴛᴀᴘ ᴏɴ ʜᴇʟᴘ ɪᴄᴏɴ <a href="https://t.me/Zaid_Updates/18">𝐇𝐞𝐥𝐩</a>`
+        `ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ᴀɴᴅ ᴀʙᴏᴜᴛ ᴄᴍᴅꜱ ᴛᴀᴘ ᴏɴ ʜᴇʟᴘ ɪᴄᴏɴ <a href="https://t.me/ironmansupportgroup/18">𝐇𝐞𝐥𝐩</a>`
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
-            [Markup.button.url('ᴀᴅᴅ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ', 'https://t.me/Zaid2_Robot')]
+            [Markup.button.url('ᴀᴅᴅ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ', 'https://t.me/ironma_group_assist_bot')]
         ]),
         disable_web_page_preview: true
     })
